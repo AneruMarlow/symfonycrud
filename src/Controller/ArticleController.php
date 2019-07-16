345ef0eb6831216c179f80e531b309bc51c7f2de
AneruMarlow <gruviafromm@gmail.com>
2019-07-16 14:12:17 +0300

    fixup! article controller 1.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,7 +6,6 @@ use App\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 
 class ArticleController extends AbstractController
